3e720da927baf3fd2b1681eac10997383ea208d4
Eduardo Leite <leite.eduardo@academico.ifpb.edu.br>
2022-03-23 15:58:21 -0300

    Update index.js

--- aula12/index.js
+++ aula12/index.js
@@ -2,4 +2,12 @@ let valorA = 'a'
 let valorB = 'b' 
 let valorC = 'c'
 
-console.log(valorA, valorB, valorC)
+const tempvalorA = valorA
+
+valorA = valorB
+valorB = valorC
+valorC = tempvalorA
+
+console.log(valorA , valorB , valorC)
+
+//aprendi sobre variaveis temporarioas
